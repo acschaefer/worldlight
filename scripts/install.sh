193538d18731960c9ff20c21b4ba08c27267d05b
Alexander Schaefer <acschaefer@users.noreply.github.com>
2018-09-29 14:43:17 -0700

    Add script stubs.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,1 +1,26 @@
+#!/bin/bash
+
+# Set the colors used to structure the console output.
+
+Black        0;30     Dark Gray     1;30
+Red          0;31     Light Red     1;31
+Green        0;32     Light Green   1;32
+Brown/Orange 0;33     Yellow        1;33
+Blue         0;34     Light Blue    1;34
+Purple       0;35     Light Purple  1;35
+Cyan         0;36     Light Cyan    1;36
+Light Gray   0;37     White         1;37
+
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+echo -e "I ${RED}love${NC} Stack Overflow"
+
+# Install PHP in order to be able to run the script that generates the worldlight image.
 sudo apt-get install php7.0-gd
+
+# Get the path to the worldlight directory.
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+# Add 
+echo 'update_wallpaper.sh' >> /etc/rc.local
---
+++ scripts/update_wallpaper.sh
@@ -0,0 +1,8 @@
+while :
+do
+    sleep 1m
+  # loop infinitely
+done
+
+php worldlight.php
+gsettings set org.gnome.desktop.background picture-uri "file:///home/username/path/to/image.jpg"
